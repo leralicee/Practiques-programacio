761501c6177e2a6828643b185705a9ab1dc0e025
Paula <alu.paula.romero@mataro.epiaedu.cat>
2025-09-30 16:47:32 +0200

    arreglada estructura arxius

--- PE02/src/HistoriaVarisFinals.java
+++ PE02/src/HistoriaVarisFinals.java
@@ -1,14 +1,10 @@
-package PE02;
+package PE02.src;
+
 import java.util.Scanner;
 
 public class HistoriaVarisFinals {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-
-
-
-
         
         scanner.close();
     }
