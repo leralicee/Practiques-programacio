d6048e7490e0e8af4fd47f38d82fc278348d2742
Paula <alu.paula.romero@mataro.epiaedu.cat>
2025-09-30 17:48:11 +0200

    finalitzada decisio 0

--- PE02/src/HistoriaVarisFinals.java
+++ PE02/src/HistoriaVarisFinals.java
@@ -6,6 +6,92 @@ public class HistoriaVarisFinals {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
+        //Variables to store decisions
+        boolean hasWatch = false;
+        boolean hasConfessed = false;
+        boolean hasAskedAbtMagic = false;
+        boolean hasTriedWatch = false;
+        boolean hasAcceptedCoin = false;
+        boolean hasFollowedMap = false;
+        String finalEnding = "";
+
+        //Start of the story
+        System.out.println("--- The Mystery of the Antique Watch ---\n");
+        System.out.println("You are in an antique shop. The owner shows you a very old pocket watch with a strange engraved symbol.");
+        System.out.println("Suddenly, he receives a phone call and has to leave, leaving you alone in the shop.\n");
+
+        //Decision 0
+        System.out.println("Do you want to take the watch without permission? (yes/no)");
+        String decision0 = scanner.nextLine().toLowerCase();
+        if (decision0.equals("yes")){
+            hasWatch = true;
+            System.out.println("You took the watch. The owner returns and looks at you suspiciously.\n");
+
+            //Decision 1.1
+            System.out.println("Do you want to confess to taking the watch? (yes/no)");
+            String decision1 = scanner.nextLine().toLowerCase();
+            if (decision1.equals("yes")){
+                hasConfessed = true;
+                System.out.println("The owner is surprised but appreciates your honesty. He tells you the watch is rumored to have magical properties.\n");
+
+                //Decision 1.1.1
+                System.out.println("Do you want to ask about the magic of the watch? (yes/no)");
+                String decision2 = scanner.nextLine().toLowerCase();
+                if (decision2.equals("yes")){
+                    hasAskedAbtMagic = true;
+                    System.out.println("The owner explains that the watch can manipulate time, but only for 5 minutes.\n");
+
+                    //Decision 1.1.1.1
+                    System.out.println("Do you want to try using the watch's magic? (yes/no)");
+                    String decision3 = scanner.nextLine().toLowerCase();
+                    if (decision3.equals("yes")){
+                        hasTriedWatch = true;
+                        finalEnding = "Heroic";
+                        System.out.println("You used the watch to go back in time and prevent an accident occurring outside. HEROIC ENDING!");
+                    } else {
+                        finalEnding = "Mysterious";
+                        System.out.println("The owner gifts you the watch, but you never end up using it. MYSTERIOUS ENDING!");
+                    }
+                } else {
+                    finalEnding = "Neutral";
+                    System.out.println("You return the watch and leave the shop. NEUTRAL ENDING!");
+                }
+            } else {
+                finalEnding = "Tragic";
+                System.out.println("You try to hide it, but the owner notices and you are arrested for theft. TRAGIC ENDING!");
+            }
+        
+        } else {
+            System.out.println("You decide not to take the watch. The owner returns and rewards you with an old coin.\n");
+            
+            //Decision 2
+            System.out.println("Do you want to accept the coin? (yes/no)");
+            
+
+        }
+        
+        
+        
+        // Results of the story
+        System.out.println("\n--- Summary of your adventure ---\n");
+        System.out.println("Ending obtained: " + finalEnding);
+        System.out.println("Has taken the watch: " + hasWatch);
+        
+        if (hasWatch) {
+            System.out.println("Has confessed: " + hasConfessed);
+            if (hasConfessed) {
+                System.out.println("Has asked about magic: " + hasAskedAbtMagic);
+                if (hasAskedAbtMagic) {
+                    System.out.println("Has tried the watch: " + hasTriedWatch);
+                }
+            }
+        } else {
+            System.out.println("Has accepted the coin: " + hasAcceptedCoin);
+            if (hasAcceptedCoin) {
+                System.out.println("Has followed the map: " + hasFollowedMap);
+            }
+        }
+
         scanner.close();
     }
 }
