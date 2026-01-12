3793ba9144f648e72932cd23dff06d605991a6d3
Paula <alu.paula.romero@mataro.epiaedu.cat>
2026-01-12 17:15:54 +0100

    pe07

---
+++ PE07/Escacs.java
@@ -0,0 +1,10 @@
+public class Escacs {
+    public static void main(String[] args) {
+        Escacs app = new Escacs();
+        app.iniciar();
+    }
+
+    public void iniciar() {
+        
+    }
+}
